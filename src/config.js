189ba7212aeaf779ae39e0537892a33e486af1f8
ongEiEi <ong54321@hotmail.com>
2019-05-18 21:14:09 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 module.exports = {
-    port_server1: process.env.HTTP_PORT1 || 8080,
-    port_server2: process.env.HTTP_PORT2 || 8081,
+    port_server1: process.env.HTTP_PORT1 || 5000,
+    port_server2: process.env.HTTP_PORT2 || 6000,
     db: {
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
